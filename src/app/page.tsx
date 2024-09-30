8054039253c1396eee97966f6be9fb97e9efc9a1
clemensgoering <clemens.goe@gmx.de>
2024-09-30 23:48:51 +0200

    Updated Configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
 export default function Home() {
     return (
         <main>
-                <HeroSection/>
             <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
                 <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+                <HeroSection/>
                     <Sections/>
                     <Pricing/>
                     <Rating/>
