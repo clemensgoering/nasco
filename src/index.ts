efd69c45ee73db62402fa5df2ddad0f8f0cc2773
clemensgoering <clemens.goe@gmx.de>
2024-10-01 00:17:19 +0200

    Updated Components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Button from "./components/Button";
-import Header from "./components/Header";
+import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
 import { HeroImage, 
-         HeroSimple } from "./components/Hero";
+         HeroSimple } from "./components/Hero/Hero";
 import Card from "./components/Card/Card";
 import Image from "./components/Placeholder/Image";
 
